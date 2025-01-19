559c4b609fd32684230c1a066492df38c1795517
pedrocassioG <pedrocassio.gb@gmail.com>
2025-01-19 13:34:27 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function verificarChute() {
     if (chute == numeroSecreto){
         exibirTextoNaTela('h1', 'acertou!');
         let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
-        let mensagemTentativas = `parabéns! vc acertou o número secreto com ${tentativas} ${palavraTentativa}`;
+        let mensagemTentativas = `parabéns! você acertou o número secreto com ${tentativas} ${palavraTentativa}`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
         }else { if(chute < numeroSecreto){
